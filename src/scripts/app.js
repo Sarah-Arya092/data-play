8185b4bb80f24e91f0f691c4030a8b28bda8c74b
Sarah_Arya092 <sarah.1997@live.be>
2022-03-29 16:42:55 +0200

    modif css

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,9 @@
-console.info('Hello world');
+"use strict";
+
+fetch('../assets/sleep.json')
+ .then((response) => {
+     console.log('Success!');
+ }, (response) => {
+    console.log('Error :(');
+ });
+
