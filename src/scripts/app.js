3555532f60fb77999d1f21b991c58cdce2724670
Hannah Mathot <hannah.mathot@laposte.net>
2022-03-30 15:51:00 +0200

    footer, bouton + js bouton, améliorations minimes, class pour json

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,10 @@
 console.info('Hello world');
+
+let blurBox = document.querySelector('.questions');
+let closeButton = document.querySelector('.questions__close');
+
+closeButton.addEventListener('click', closeBlur);
+
+function closeBlur(){
+    blurBox.classList.add('questions--none');
+}
